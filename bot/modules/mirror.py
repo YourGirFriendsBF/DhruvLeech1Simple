0a66600ffb3f25fda157615d3f8c5866197a0f0b
YourGirFriendsBF <109566119+YourGirFriendsBF@users.noreply.github.com>
2022-08-05 02:25:08 +0530

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -77,7 +77,7 @@ class MirrorListener:
         if self.isZip:
             try:
                 with download_dict_lock:
-                    download_dict[self.uid] = ZipStatus(self, name, path, size)
+                    download_dict[self.uid] = ZipStatus(name, m_path, aize)
                 path = m_path + ".zip"
                 LOGGER.info(f'Zip: orig_path: {m_path}, zip_path: {path}')
                 if self.pswd is not None:
