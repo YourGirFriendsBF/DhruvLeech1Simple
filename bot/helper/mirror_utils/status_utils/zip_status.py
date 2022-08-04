2d85d1b9e2403a2bb9f7a30eae06c7efa7da1ecf
YourGirFriendsBF <109566119+YourGirFriendsBF@users.noreply.github.com>
2022-08-05 02:15:45 +0530

    Update zip_status.py

--- bot/helper/mirror_utils/status_utils/zip_status.py
+++ bot/helper/mirror_utils/status_utils/zip_status.py
@@ -1,50 +1,32 @@
-from time import time
-from bot import DOWNLOAD_DIR, LOGGER
-from bot.helper.ext_utils.bot_utils import get_readable_file_size, MirrorStatus, get_readable_time
-from bot.helper.ext_utils.fs_utils import get_path_size
+from bot.helper.ext_utils.bot_utils import get_readable_file_size, MirrorStatus
 
 
 class ZipStatus:
-    def __init__(self, name, size, gid, listener, message):
+    def __init__(self, name, path, size):
         self.__name = name
-        self.__gid = gid
+        self.__path = path
         self.__size = size
-        self.__listener = listener
-        self.__start_time = time()
-        self.message = listener.message
-        self.message = message
 
     def progress(self):
         return '0'
-        return f'{round(self.progress_raw(), 2)}%'
 
     def speed(self):
         return '0'
-        return f'{get_readable_file_size(self.speed_raw())}/s'
-    
-    def size_raw(self):
-        return self.__size
-    
+
     def name(self):
         return self.__name
 
     def path(self):
         return self.__path
-    def size_raw(self):
-        return self.__size
+
     def size(self):
         return get_readable_file_size(self.__size)
 
     def eta(self):
         return '0s'
-        try:
-            seconds = (self.size_raw() - self.processed_bytes()) / self.speed_raw()
-            return f'{get_readable_time(seconds)}'
-        except:
-            return '-'
 
     def status(self):
-        return MirrorStatus.STATUS_SPLITTING
+        return MirrorStatus.STATUS_ARCHIVING
 
     def processed_bytes(self):
         return 0
